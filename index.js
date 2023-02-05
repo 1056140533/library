11bd0b2914ed015a80baac0f2fbe38399f7b37e1
wanghaibo <1056140533@qq.com>
2023-02-05 22:18:04 +0800

    feat: 新增读取文件生成json的脚本

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const marked = require('marked')
+const cheerio = require('cheerio');
+const files = fs.readdirSync('./article')
+let jsonData = []
+files.forEach(( file) => {
+    const articles = fs.readdirSync(`./article/${file}`)
+    articles.forEach(article=>{
+        const data = fs.readFileSync(`./article/${file}/${article}`, 'utf8');
+        const text = marked.parse(data)
+        const $ = cheerio.load(text);
+        jsonData.push({
+            id:article,
+            type:file,
+            title:$('h1').text(),
+            time:$('p.time').text()
+        })
+    })
+})
+let content = {
+    data: {
+        total: 100,
+        list:jsonData
+    },
+    code: 0,
+    message: "success"
+}
+fs.writeFile('./article.json', JSON.stringify(content), function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('文件创建成功，地址：');
+});
+
+
